c32215eabf62d5f8cc2c4d5f979f45fc215b16cf
Yudin Dennis <yudin@1mps.ru>
2024-12-25 11:57:09 +0300

    init commit

---
+++ src/test/java/dev/yudin/kedat/KedatApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yudin.kedat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KedatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
